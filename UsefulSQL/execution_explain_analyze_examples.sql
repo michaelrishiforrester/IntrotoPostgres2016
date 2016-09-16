0938268f6687a0dc00c854e71b43770479ec3ac4
Michael R Forrester <itamazonian@gmail.com>
2016-09-16 17:45:45 -0400

    added datasets

---
+++ UsefulSQL/execution_explain_analyze_examples.sql
@@ -0,0 +1,5 @@
+dvdrental=# EXPLAIN select * from payment where amount > 4.99;
+
+dvdrental=# EXPLAIN ANALYZE select * from payment where amount > 4.99;
+
+dvdrental=# EXPLAIN VERBOSE select * from payment where amount > 4.99;
